21899372b61548a4b44cd4cc146f354786901158
AndersMunk <munk.anders@gmail.com>
2019-11-22 20:58:51 +0100

    Update __main__.py

--- ankisyncd/__main__.py
+++ ankisyncd/__main__.py
@@ -6,6 +6,8 @@ if __package__ is None and not hasattr(sys, "frozen"):
     sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
 
 import ankisyncd.sync_app
+import ankisyncd.rest_app
 
 if __name__ == "__main__":
     ankisyncd.sync_app.main()
+    ankisynce.rest_app.main()
